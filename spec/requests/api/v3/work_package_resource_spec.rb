048d53bd4e733c3dfbdddf21d3dca1e4b45bd20c
Alexander Bach <a.bach@finn.de>
2015-03-10 10:41:46 +0100

    Added specs for estimated hours and percentage done

--- spec/requests/api/v3/work_package_resource_spec.rb
+++ spec/requests/api/v3/work_package_resource_spec.rb
@@ -756,7 +756,7 @@ h4. things we like
 
           it_behaves_like 'multiple errors', 422
 
-          it_behaves_like 'multiple errors of the same type', 2, 'PropertyConstraintViolation'
+          it_behaves_like 'multiple errors of the same type', 2, 'PropertyIsReadOnly'
 
           it_behaves_like 'multiple errors of the same type with messages' do
             let(:message) { ['Subject can\'t be blank.', 'Parent does not exist.'] }
@@ -804,7 +804,7 @@ h4. things we like
 
           it_behaves_like 'multiple errors', 422, 'Multiple fields violated their constraints.'
 
-          it_behaves_like 'multiple errors of the same type', 2, 'PropertyConstraintViolation'
+          it_behaves_like 'multiple errors of the same type', 2, 'PropertyIsReadOnly'
 
           it_behaves_like 'multiple errors of the same type with messages' do
             let(:message) {
